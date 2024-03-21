432c4346f6c161efc23f132a2d7c538f79fc3769
Blake Graviett <blake.graviett@gmail.com>
2024-03-21 18:42:05 -0500

    Add basic UI

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
 	selector: "app-register",
 	templateUrl: "./register.component.html",
 	styleUrl: "./register.component.scss"
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+	// CREATE LOGIN FORM
+	registerForm = new FormGroup({
+		username: new FormControl("", [Validators.required]),
+		email: new FormControl("", [Validators.required, Validators.email]), // make sure it is a valid email address
+		password: new FormControl("", [Validators.required])
+	});
+
+	// On Submit Function
+	onSubmit() {
+		// TODO: Logic once form is submitted
+		console.log(this.registerForm.value);
+	}
+}
