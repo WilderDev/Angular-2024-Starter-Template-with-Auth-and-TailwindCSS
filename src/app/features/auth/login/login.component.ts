dd5029ddcfa4c9a3ec0c0ced98a9bbaee36cefce
Blake Graviett <blake.graviett@gmail.com>
2024-03-21 18:33:26 -0500

    INIT

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrl: "./login.component.scss"
 })
 export class LoginComponent {
+	// CREATE LOGIN FORM
+	loginForm = new FormGroup({
+		email: new FormControl("", [Validators.required, Validators.email]), // make sure it is a valid email address
+		password: new FormControl("", [Validators.required])
+	});
 
+	// On Submit Function
+	onSubmit() {
+		// TODO: Logic once form is submitted
+		console.log(this.loginForm.value);
+	}
 }
